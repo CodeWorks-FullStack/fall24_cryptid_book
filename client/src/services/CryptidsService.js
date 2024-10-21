36f5ee78d1c6975b83b6eb0853cba356a8487d47
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-21 13:16:12 -0600

    storing cryptids in appstate

--- client/src/services/CryptidsService.js
+++ client/src/services/CryptidsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Cryptid } from "@/models/Cryptid.js"
+import { AppState } from "@/AppState.js"
 
 class CryptidsService {
   async getAllCryptids() {
     const response = await api.get('api/cryptids')
     logger.log('GOT CRYPTIDS 👹👽🧌', response.data)
+    const cryptids = response.data.map(cryptidPOJO => new Cryptid(cryptidPOJO))
+    AppState.cryptids = cryptids
   }
 
 }
